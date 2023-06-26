6dcb613a243e45f0e747aab873d93b6ab3ec9d36
RaftDev <theraft08@gmail.com>
2023-06-26 23:59:30 +0200

    First lines of code

---
+++ src/main/java/be/raft/launcher/CraftedLauncher.java
@@ -0,0 +1,70 @@
+/*
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ *
+ * This software is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License ("License").
+ * You should have received a copy of the License along with this software.
+ * If not, please visit: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * Summary of the License:
+ * - You are free to:
+ *     - Share: Copy and redistribute the software in any medium or format.
+ *     - Adapt: Remix, transform, and build upon the software.
+ *
+ * Under the following terms:
+ * - Attribution:
+ *     - You must give appropriate credit to the original author (RaftDev) and provide a reference to the license.
+ * - Non-Commercial:
+ *     - You may not use this software for commercial purposes.
+ * - Share-Alike:
+ *     - If you remix, transform, or build upon this software, you must distribute your contributions under the same license as the original.
+ *
+ * For detailed license terms and conditions, please refer to the included License file or visit: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * © 2023 RaftDev. All Rights Reserved.
+ * Crafted-Launcher is a project by RaftDev and is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ */
+
+package be.raft.launcher;
+
+import be.raft.launcher.misc.LaunchArguments;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+
+public class CraftedLauncher {
+    public static final Logger logger = LoggerFactory.getLogger("Crafted-Launcher");
+    public static LaunchArguments launchArgs;
+
+    public static void main(String[] args) {
+        CraftedLauncher.logger.info("Validating environment..");
+
+        //Env validation
+        if (validateJavaFx()) {
+            return;
+        }
+
+        //Argument parsing
+        launchArgs = LaunchArguments.buildArguments(args);
+
+        //Misc
+        System.setProperty("prism.lcdtext", "false"); //Enhance smoothness of the text
+    }
+
+    private static boolean validateJavaFx() {
+        try {
+            Class.forName("javafx.application.Application");
+            CraftedLauncher.logger.info("JavaFX validation successful!");
+            return true;
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Error:\n" + e.getMessage() + "\nnot found!" +
+                    "\nUnable to start the launcher!", "error", JOptionPane.ERROR_MESSAGE);
+            CraftedLauncher.logger.error("JavaFX validation failed!", e);
+            return false;
+        }
+    }
+
+    public static LaunchArguments getLaunchArgs() {
+        return launchArgs;
+    }
+}
---
+++ src/main/java/be/raft/launcher/misc/Environment.java
@@ -0,0 +1,78 @@
+/*
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ *
+ * This software is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License ("License").
+ * You should have received a copy of the License along with this software.
+ * If not, please visit: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * Summary of the License:
+ * - You are free to:
+ *     - Share: Copy and redistribute the software in any medium or format.
+ *     - Adapt: Remix, transform, and build upon the software.
+ *
+ * Under the following terms:
+ * - Attribution:
+ *     - You must give appropriate credit to the original author (RaftDev) and provide a reference to the license.
+ * - Non-Commercial:
+ *     - You may not use this software for commercial purposes.
+ * - Share-Alike:
+ *     - If you remix, transform, or build upon this software, you must distribute your contributions under the same license as the original.
+ *
+ * For detailed license terms and conditions, please refer to the included License file or visit: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * © 2023 RaftDev. All Rights Reserved.
+ * Crafted-Launcher is a project by RaftDev and is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ */
+
+package be.raft.launcher.misc;
+
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
+
+public class Environment {
+    private final OperatingSystem os;
+
+    private Environment(OperatingSystem os) {
+        this.os = os;
+    }
+
+    public long getAvailableMemory() {
+        return ((OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getFreeMemorySize();
+    }
+
+    public long getTotalMemory() {
+        return ((OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalMemorySize();
+    }
+
+    public OperatingSystem getOs() {
+        return os;
+    }
+
+    //Static
+    private static Environment environment;
+
+    public static Environment getEnvironment() {
+        if (environment == null) {
+            environment = new Environment(OperatingSystem.getOs());
+        }
+
+        return environment;
+    }
+
+    public enum OperatingSystem {
+        LINUX, WINDOWS, MACOS;
+
+        public static OperatingSystem getOs() {
+            String osStr = System.getProperty("os.name");
+
+            if (osStr.contains("win")) {
+                return WINDOWS;
+            } else if (osStr.contains("mac")) {
+                return MACOS;
+            } else {
+                return LINUX;
+            }
+        }
+    }
+}
