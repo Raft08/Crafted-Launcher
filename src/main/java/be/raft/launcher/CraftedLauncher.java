204f2f2fe829b346a0d7bba088dec123c2c06eda
RaftDev <theraft08@gmail.com>
2024-06-16 14:30:38 +0200

    Initial Commit

---
+++ src/main/java/be/raft/launcher/CraftedLauncher.java
@@ -0,0 +1,11 @@
+package be.raft.launcher;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CraftedLauncher extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
