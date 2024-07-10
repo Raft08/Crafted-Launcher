f636ae17969c05d0638fc626dc8c502f8874ce57
RaftDev <theraft08@gmail.com>
2024-07-10 22:13:54 +0200

    Added run tasks

--- src/main/java/be/raft/launcher/CraftedLauncher.java
+++ src/main/java/be/raft/launcher/CraftedLauncher.java
@@ -1,11 +1,17 @@
 package be.raft.launcher;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class CraftedLauncher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new GridPane());
+        stage.setScene(scene);
+        stage.setTitle("Crafted Launcher");
 
+        stage.show();
     }
 }
